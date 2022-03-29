7c35e1d1471404991471eff84519448666dbe48e
BJ <bjadams31@gmail.com>
2022-03-28 21:00:15 -0400

    Add base format.

--- falsesouth_dashboard.py
+++ falsesouth_dashboard.py
@@ -2,4 +2,30 @@ import numpy as np
 import pandas as pd 
 import streamlit as st 
 
-st.write("Hello World!")
+########## Set Page Config Parameters ##########
+st.set_page_config(
+     page_title="FalseNorth Insights",
+     layout="wide",
+     initial_sidebar_state="expanded",)
+
+########## Data Management ##########
+invoice_df = pd.read_excel('./data/falsesouth_receivables.xlsx')
+
+########## Sidebar ##########
+st.sidebar.image('./data/img/logo.png', width=300)
+
+st.sidebar.markdown("----")
+
+st.sidebar.markdown('<b><p style="font-size: 29px; text-align: center; color:#d3755c;">Broker Selections</p></b>', unsafe_allow_html=True)
+
+
+########## Header ##########
+st.markdown('<b><p style="font-size: 65px; text-align: center; color:#1d9385;">FalseSouth - Receivables Insights</p></b>', unsafe_allow_html=True)
+
+
+########## Body ##########
+
+# Top Level Quotes and Summary
+st.markdown('<b><p style="font-size: 30px; text-align: left; color:#1d9385;">Invoice Data</p></b>', unsafe_allow_html=True)
+
+st.dataframe(invoice_df.sample(8))
